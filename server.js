b1f8881428b7f49175d91fb249d5dc72754a1946
Rob Korich <tennisrob@gmail.com>
2020-08-01 22:24:13 -0700

    added fs readfile to js

--- server.js
+++ server.js
@@ -21,5 +21,16 @@ http.createServer((request, response) => {
     }
   });
 
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      throw err;
+    }
+
+    response.writeHead(200, { 'Content-Type': 'text/html' });
+    response.write(data);
+    response.end();
+
+  });
+  
 }).listen(8080);
 console.log('My test server is running on Port 8080.');

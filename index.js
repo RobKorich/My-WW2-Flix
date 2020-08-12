0c15b3e69a64518a3d43e726184fbed4582c8fe9
Rob Korich <tennisrob@gmail.com>
2020-08-11 19:59:10 -0700

    documentation update and express routing

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const express = require('express');
 const app = express();
 //import morgan for logging
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const uuid = require('uuid');
+
 
 let ww2Movies = [
     {
@@ -16,6 +19,7 @@ let ww2Movies = [
 app.use(morgan('common'));
 //invoke express static function (routes all requests for static files to their corresponding files within 'public' folder)
 app.use(express.static('public'));
+app.use(bodyParser.json());
 //invoke error handling middleware function
 app.use((err, req, res, next) => {
     console.error(err.stack);
@@ -25,6 +29,57 @@ app.use((err, req, res, next) => {
 //express routing syntax (Express GET route endpoint /movies)
 app.get('/movies', (req, res) => {
     res.json(ww2Movies);
+});
+
+//GET data about single movie by title
+app.get('/movies/:title', (req, res) => {
+    res.json(ww2Movies.find((movie) =>
+      { return movie.title === req.params.title }));
+});
+
+//GET data about movie genre to user
+app.get('/movies/genres/:name', (req, res) => {
+    res.send('Successful GET request returning data on movie genre');
+});
+
+//GET data about movie director to user
+app.get('/movies/directors/:name', (req, res) => {
+    res.send('Successful GET request returning data on movie director');
+});
+
+//POST new user account
+app.post('/users', (req, res) => {
+    res.send('Successful POST of new user registration');
+    /*let newUser = req.body;
+  
+    if (!newUser.name) {
+      const message = 'Missing name in request body';
+      res.status(400).send(message);
+    } else {
+      newUser.id = uuid.v4();
+      users.push(newUser);
+      res.status(201).send(newUser);
+    }*/
+  });
+
+//Update user information
+app.put('/users/:username', (req, res) => {
+    res.send('Successful PUT of new user data');
+});
+
+//POST movie to user's favorites list
+app.post('/users/:username/favorites/:movieID', (req, res) => {
+    res.send('Successful POST of movie to user\'s favorite\'s list');
+});
+
+//DELETE movie from user's favorite's list
+app.delete('/users/:username/favorites/:movieID', (req, res) => {
+    res.send('Successful DELETE of movie from user\'s favorite\'s list');
+});
+
+//DELETE user's account
+app.delete('/users/:username', (req, res) => {
+    res.send('Successful DELETE of user\'s account');
 });
 
 //Express GET returns text response at endpoint '/'

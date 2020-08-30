37a4b840daae3c1ecfc1fdc219a93358e28c4fbf
Rob Korich <tennisrob@gmail.com>
2020-08-29 17:49:52 -0700

    commit message

--- index.js
+++ index.js
@@ -265,7 +265,7 @@ app.delete('/users/:Username', passport.authenticate('jwt', {session: false}), (
 });
 
 //Express GET returns text response at endpoint '/'
-app.get('/', passport.authenticate('jwt', {session: false}), (req, res) => {
+app.get('/', (req, res) => {
     res.send('Welcome to My WW2 Flix!');
 });
 
